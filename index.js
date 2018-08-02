d35d8be7a78b775011f00affbb4b84e47578d000
Thomas Chun <tchun2055@gmail.com>
2018-08-02 01:27:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,7 @@ function sayHiToGrandma(string) {
     return "YES INDEED!"
   } else if (string === "I love you, Grandma.") {
     return "I love you, too."
+  } else if (string.toLowerCase() === string) {
+    return "I can't hear you!"
   }
 }
