7855a156c4f042b4c6c2ce71faf9be8b6ae4fc38
Thomas Chun <tchun2055@gmail.com>
2018-08-02 01:28:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,11 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   }
 }
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
